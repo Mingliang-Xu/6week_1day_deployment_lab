541ab8cf43fbca94aea0970ec0762816a92e312d
Mingliang-Xu <oliver.xml@hotmail.com>
2023-10-16 14:33:54 -0700

    6week 1day deployment lab initialized

---
+++ index.js
@@ -0,0 +1,15 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.json());
+
+app.use(express.static(__dirname + '/public'))
+
+
+
+
+
+app.listen(4000, ()=>{console.log('server up on port 4000')});
+
+
